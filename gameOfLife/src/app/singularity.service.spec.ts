3d343ff45630d34311f56410d11a5b0b4e42329a
Jonathan Günz <jonathan.guenz@outlook.com>
2017-11-18 10:13:12 +0100

    test to check if a cell will survive in singularity

---
+++ gameOfLife/src/app/singularity.service.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { SingularityService } from './singularity.service';
+
+describe('SingularityService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SingularityService]
+    });
+  });
+  
+    it('should be created', inject([SingularityService], (service: SingularityService) => {
+      expect(service).toBeTruthy();
+    }));
+    
+      it('Does GetCellState exist', inject([SingularityService], (service: SingularityService) => {
+        
+        
+        
+        expect(service.GetCellState != null).toBeTruthy();
+      }));
+});
---
+++ gameOfLife/src/app/singularity.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SingularityService {
+
+  constructor() { }
+
+}
